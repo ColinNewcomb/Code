2135b9783f90138f8622cf9470886a37890bb9e4
Colin Newcomb <cnewc1115@gmail.com>
2024-01-30 16:55:39 -0500

    new

--- CollegeCode-main/Code depository/Second Semester/test.java
+++
@@ -1,9 +0,0 @@
-class Test {
-    public static void main(String[] args) {
-        int myInt= 7;
-        char Initials = 'c';
-
-        System.out.println(myInt);
-        System.out.println(Initials);
-    }
-}
